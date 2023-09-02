733168de31567245c256064ed55bd0ec419530fc
John Doe <test@email.com>
2023-09-02 03:07:52 +0200

    feature/dnd

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { Router } from 'components/router/router';
 import { AuthProvider } from 'context/auth';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
   return (
     <AuthProvider>
-      <Router />
       <ToastContainer
         position="bottom-center"
         autoClose={4000}
@@ -19,6 +20,9 @@ function App() {
         pauseOnHover
         theme="dark"
       />
+      <DndProvider backend={HTML5Backend}>
+        <Router />
+      </DndProvider>
     </AuthProvider>
   );
 }
--- src/components/tracker/tracker.tsx
+++ src/components/tracker/tracker.tsx
@@ -1,3 +1,6 @@
+//libs
+import { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 //styles
 import s from './tracker.module.css';
 
@@ -13,21 +16,93 @@ export type Tracker = {
   reward?: string;
 };
 
+interface DragItem {
+  index: number;
+  id: string;
+  type: string;
+}
+interface XYCoord {
+  x: number;
+  y: number;
+}
+
 export type TrackerProps = {
+  index: number;
   tracker: Tracker;
   onEdit: (tracker: Tracker) => void;
   onDelete: (id: string) => void;
+  onMove: (dragIndex: number, hoverIndex: number) => void;
 };
 
-export function Tracker({ tracker, onEdit, onDelete }: TrackerProps) {
+export function Tracker({ index, tracker, onEdit, onDelete, onMove }: TrackerProps) {
+  const dragRef = useRef<HTMLButtonElement>(null);
+  const previewRef = useRef<HTMLDivElement>(null);
+
+  const [{ handlerId }, drop] = useDrop<DragItem, void, { handlerId: string | symbol | null }>({
+    accept: 'card',
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item: DragItem, monitor) {
+      if (!previewRef.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      const hoverBoundingRect = previewRef.current?.getBoundingClientRect();
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      onMove(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag, preview] = useDrag({
+    type: 'card',
+    item: () => {
+      return { id: tracker.id, index };
+    },
+    collect: (monitor: any) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  drag(dragRef);
+  drop(preview(previewRef));
+
   const editClick = () => onEdit(tracker);
   const deleteClick = () => onDelete(tracker.id);
 
   return (
-    <div className={s.box}>
+    <div
+      className={s.box}
+      ref={previewRef}
+      style={{ opacity: isDragging ? 0 : 1 }}
+      data-handler-id={handlerId}
+    >
       <div>
         <button onClick={deleteClick}>delete</button>
         <button onClick={editClick}>edit</button>
+        <button ref={dragRef} className={s.moveButton}>
+          move
+        </button>
       </div>
       <div className={s.fields}>
         <span>Name: {tracker.name}</span>
